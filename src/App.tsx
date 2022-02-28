e96e5a7a3124bdd74e8f1b0285564bef0706c422
Igor Menezes <ismv00@icloud.com>
2022-02-28 18:31:00 -0400

    add react-router-dom6

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
 
 import { Home } from './pages/Home';
-import { NewRoom } from './pages/NewRoom';
+import { NewRoom } from "./pages/NewRoom";
 
 function App() {
-
   return (
-    <NewRoom />
-  );    
+    <Routes>
+        <Route path="/"  element={<Home />} />
+        <Route path="/rooms/new" element={<NewRoom />} />
+
+    </Routes>
+  );
 }
 
 export default App;
