38940e05d5084bf5880470db1371095f907068de
Igor Menezes <ismv00@icloud.com>
2022-03-26 17:07:08 -0400

    ajuste nas questions

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,9 +1,23 @@
+import cx from "classnames";
 import { QuestionProps } from "interfaces/QuestionTypes";
 import "../styles/questions.scss";
 
-export function Question({ author, children, content }: QuestionProps) {
+export function Question({
+  author,
+  children,
+  content,
+  isAnswered = false,
+  isHighLighted = false
+}: QuestionProps) {
   return (
-    <div className="question">
+    <div
+      className={cx(
+        // Utilizando o pacote classnames do npm para criar as condicoes nas classes
+        "question",
+        { answered: isAnswered },
+        { highLighted: isHighLighted && !isAnswered }
+      )}
+    >
       <p>{content}</p>
       <footer>
         <div className="user-info">
